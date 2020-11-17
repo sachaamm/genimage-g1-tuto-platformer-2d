30f0089901b75ce7d7e945421971ca27abff7fae
sachaamm <sachaamm@gmail.com>
2020-11-17 16:17:09 +0100

    Init
    Init

---
+++ Assets/Scripts/CreateWall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateWall : MonoBehaviour
+{
+    public GameObject Cube;
+    void Start()
+    {
+        int wallWidth = 5;
+        int wallHeight = 3;
+
+        for (int y = 0; y < wallHeight; y++)
+        {
+            for (int x = 0; x < wallWidth; x++)
+            {
+                GameObject newCube = Instantiate(Cube);
+                newCube.transform.position = new Vector3(x, y, 5);
+            }
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/SpawnCube.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCube : MonoBehaviour
+{
+    public GameObject cube;
+
+    void Start()
+    {
+        // Je crée un GameObject à partir de la fonction Instantiate
+        
+        // Une boucle 
+        // via trois mot clés : while et for et foreach
+        
+        // la boucle for parcoure un index 
+        // valeur de départ 0
+        // valeur maximale 100
+        // incrementation : de combien on incremente i a chaque execution de la boucle
+        for (int i = 0; i < 100; i++)
+        {
+            Instantiate(cube);
+        }
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
