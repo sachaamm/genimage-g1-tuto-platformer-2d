aa9f937ac11a82d2cafa80557cbf3f461ec5a022
Marco Annunziata <annunziatamarco1@gmail.com>
2020-11-24 16:41:50 +0100

    EndLevel

---
+++ Assets/Scripts/EndLevel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        GameObject objectCollision = col.gameObject;
+        if (objectCollision.tag == "EndLevel")
+        {
+            // Si player entre en contacte avec DeadZone, respawn
+            LevelEnd();
+        }
+    }
+    void LevelEnd()
+    {
+        print("finNiveau");
+    }
+}
