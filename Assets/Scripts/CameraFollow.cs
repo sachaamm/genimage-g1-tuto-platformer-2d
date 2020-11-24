dee02b762c077978d143e437f29ba18a4ee56fc2
Hedidjourhi <74610752+Hedidjourhi@users.noreply.github.com>
2020-11-24 14:42:52 +0100

    camPos

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    GameObject player;
+    
+    void Start()
+    {
+        player = GameObject.FindWithTag("player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 camPos = transform.position;
+        Vector3 playerpos = player.transform.position;
+        Vector3 newCamPos = new Vector3(player.transform.position.x , player.transform.position.y, camPos.z );
+        transform.position = newCamPos;
+    }
+}
