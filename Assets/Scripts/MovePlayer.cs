98487559ac9ace106b6c347db86fdee1423d1ebb
Khyl-git <74610810+Khyl-git@users.noreply.github.com>
2020-11-24 10:11:19 +0100

    IsGrounded

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -8,7 +8,7 @@ public class MovePlayer : MonoBehaviour
     public float vitesseDeplacement = 0.05f;
     public float jumpPower = 0.2f;
     public Rigidbody2D rg;
-
+    bool isGrounded = true;
         
     // Start is called before the first frame update
     void Start()
@@ -22,21 +22,30 @@ public class MovePlayer : MonoBehaviour
         // Si j'appuie sur la fleche de gauche
         // Je deplace le joueur à gauche
         Vector3 playerpos = transform.position;
-        if( Input.GetKey( KeyCode.LeftArrow))
+        if( Input.GetKey( KeyCode.LeftArrow) && isGrounded)
         {
            // rg.(new Vector3(-2, 0, 0) * vitesseDeplacement);
            rg.MovePosition(playerpos + new Vector3(-2, 0, 0) * vitesseDeplacement);
 
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) && isGrounded)
         {
            // player.transform.Translate(new Vector3(2, 0, 0) * vitesseDeplacement);
            rg.MovePosition(playerpos + new Vector3(2, 0, 0) * vitesseDeplacement);
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             // player.transform.Translate(new Vector3(0, 1, 0) * jumpPower);
             rg.AddForce(new Vector3(0, 1, 0) * jumpPower);
         }
 }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+    }
 }
