507a6428ded83f011b31cee919a4a9fdda1c8c73
Hedidjourhi <74610752+Hedidjourhi@users.noreply.github.com>
2020-12-01 13:17:40 +0100

    Rotate

---
+++ Assets/Scripts/RotatePlatform.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlatform : MonoBehaviour
+{
+    Vector3 rotation = new Vector3(0, 0, 1);
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      transform.Rotate(rotation);
+
+    }
+}
