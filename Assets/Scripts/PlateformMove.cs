1ac89a91f730be88ab6da20ff8f8a2ea4abe47f4
Ruffi97 <ruffinigeoffrey@outlook.fr>
2020-12-01 10:51:19 +0100

    MovingPlateform

---
+++ Assets/Scripts/PlateformMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateformMove : MonoBehaviour
+{
+    public int cycle = 1000;
+    public float moveAmount = 1;
+    public Vector3 direction = new Vector3(1, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Si jamais "Time.FrameCount" % cycle < cycle/2: je vais vers le haut, sinon, je vais vers le bas.
+        if (Time.frameCount % cycle < cycle / 2)
+        {
+            transform.position += direction * Time.deltaTime * moveAmount * Responsive.ResponsiveHeight();
+        }
+        else
+        {
+            transform.position -= direction * Time.deltaTime * moveAmount * Responsive.ResponsiveHeight();
+        }
+    }
+}
