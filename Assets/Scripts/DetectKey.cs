890caa7a6ac50ea17a94a4f4a1b15d8bd27f7960
Khyl-git <74610810+Khyl-git@users.noreply.github.com>
2020-12-01 17:00:51 +0100

    Key

---
+++ Assets/Scripts/DetectKey.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectKey : MonoBehaviour
+{
+    public GameObject player;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // on va effacer une deadzone lorsque nous récupérons la clé
+        GameObject objectCollision = collision.gameObject;
+        // Si le player entre en contact avec une clé
+        if (objectCollision.tag == "Key")
+        {
+            Key key = objectCollision.GetComponent<Key>();
+            if (key != null)
+            {
+                GameObject deadZone = key.deadZone;
+                Destroy(deadZone);
+                Destroy(objectCollision);
+            }
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
