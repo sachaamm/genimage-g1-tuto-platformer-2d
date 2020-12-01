a42ca9aee70eb72d5128aae642051e7e44fcbd57
sachaamm <sachaamm@gmail.com>
2020-12-01 16:11:55 +0100

    Bullet & Portals

---
+++ Assets/Scripts/DetectPortals.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectPortals : MonoBehaviour
+{
+
+    // public GameObject portalB;
+    public GameObject player;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        GameObject objectCollision = col.gameObject;
+        // Si le player entre en contact avec un Portail
+        if (objectCollision.tag == "Portal")
+        {
+            Portal portal = objectCollision.GetComponent<Portal>();
+            // Si un script Portal est présent sur le portail
+            if (portal != null)
+            {
+                // il se téléporte sur le Portail associé
+                GameObject otherPortal = portal.otherPortal;
+                Vector3 otherPortalPosition = otherPortal.transform.position;
+                player.transform.position = otherPortalPosition;
+            }
+            
+        }
+    }
+    
+    
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+
+    public GameObject bulletPrefab;
+
+    public GameObject bulletSpawnPoint;
+    
+    private int counter = 0;
+    public int cycleShoot = 40;
+
+    public float shootPower = 200;
+
+    public float bulletLifetime = 5;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (counter % cycleShoot == 0)
+        {
+            GameObject newBullet = Instantiate(bulletPrefab);
+            newBullet.transform.position = bulletSpawnPoint.transform.position;
+            Rigidbody2D newBulletRg = newBullet.GetComponent<Rigidbody2D>();
+            newBulletRg.AddForce(new Vector2(0,1) * shootPower);
+            
+            Destroy(newBullet, bulletLifetime);
+        }
+    }
+}
