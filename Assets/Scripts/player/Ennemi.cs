c938c4fada709ca652d997858fd0f47a4cec3e44
Khyl-git <74610810+Khyl-git@users.noreply.github.com>
2020-12-08 18:00:54 +0100

    EnemySmoothMoveExplose

--- Assets/Scripts/player/Ennemi.cs
+++ Assets/Scripts/player/Ennemi.cs
@@ -7,6 +7,7 @@ public class Ennemi : MonoBehaviour
     GameObject player;
     public float distanceToFollow = 20;
     public float distanceToExplode = 1;
+    public float moveSpeed = 0.01f;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,14 +21,17 @@ public class Ennemi : MonoBehaviour
             float dist = Vector3.Distance(player.transform.position, transform.position);
             if ( dist < distanceToFollow)
             {
-            transform.position = Vector3.Lerp(transform.position , player.transform.position , 0.01f);
+            // transform.position = Vector3.Lerp(transform.position , player.transform.position , 0.01f);
+            Vector3 diff = player.transform.position - transform.position;
+            transform.Translate(diff.normalized * moveSpeed);
             }
             if ( dist < distanceToExplode)
             {
             //  todo
             Health health;
             health = player.GetComponent<Health>();
-            health.RecevoirDegats(1); 
+            health.RecevoirDegats(1);
+            Destroy(this.gameObject);
             }
 
     }
