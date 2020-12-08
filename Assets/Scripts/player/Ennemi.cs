74e02e7bd866bc10e3a9ff18aa0c6a8eea85c3de
Khyl-git <74610810+Khyl-git@users.noreply.github.com>
2020-12-08 16:42:47 +0100

    EnemyAggro

--- Assets/Scripts/player/Ennemi.cs
+++ Assets/Scripts/player/Ennemi.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class Ennemi : MonoBehaviour
 {
+    GameObject player;
+    public float distanceMin = 20;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+            float dist = Vector3.Distance(player.transform.position, transform.position);
+            if ( dist < distanceMin)
+            {
+            transform.position = Vector3.Lerp(transform.position , player.transform.position , 0.5f);
+            }
     }
     void OnCollisionEnter2D(Collision2D col)
     {
